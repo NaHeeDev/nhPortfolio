a4ea815dc57804a618fa12d3ce9bcb8de44bee78
NaHeeDev <128349340+NaHeeDev@users.noreply.github.com>
2023-04-26 14:12:15 +0900

    230426_01_commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
 /* *******************************************************
  * filename : main.js
  * description : 메인에만 사용되는 JS
- * date : 2022-08-08
+ * date : 2023-04-26
 ******************************************************** */
+
+// menu 이동함수
+function goToScroll(name) {
+    var location = document.querySelector("#" + name).offsetTop;
+    window.scrollTo({top: location, behavior: 'smooth'});
+}
 
 (() => {
     let yOffset = 0;
     let prevScrollHeight = 0;
     let curSection = 0; 
     let enterNewScene = false; // 새로운 section이 시작된 순간 true
-	let acc = 0.2;
-	let delayedYOffset = 0;
-	let rafId;
-	let rafState;
-
-    function textActiveFunc() {
-        
-    }
 
     const sectionInfo = [
-        {
+        {   // section01
             type:'sticky',
             heightNum : 3,
             scrollHeight:0,
@@ -29,6 +27,11 @@
                 title01 : document.querySelector('#mainSection01 .tit01'),
                 title02 : document.querySelector('#mainSection01 .tit02'),
                 title03 : document.querySelector('#mainSection01 .tit03'),
+
+                // Section02 미리 불러오기
+                sectionTitle : document.querySelector('#mainSection02 .main-section-tit'),
+                aboutImg : document.querySelector('#mainSection02 .img-box'),
+                aboutTxt : document.querySelector('#mainSection02 .txt-box'),
             },
             values:{
                 title01_opacity_on : [0, 1, { start: 0.1, end: 0.2 }],
@@ -51,47 +54,78 @@
 
                 visual_title_opacity_on : [0, 1, {start:0, end:0.2}],
                 visual_title_opacity_off : [1, 0, {start:0.1, end:0.2}],
+
+                // Section02 미리 불러오기
+                sectionTitle_opacity_on : [0, 1, { start: 0.8, end: 1 }],
+                sectionTitle_opacity_off: [1, 0, { start: 1, end: 1 }],
+                sectionTitle_translateY_on: [100, 0, { start: 0.8, end: 1 }],
+				sectionTitle_translateY_off: [0, -100, { start: 1, end: 1 }],
+
+                aboutImg_opacity_on : [0, 1, { start: 0.8, end: 1 }],
+                aboutImg_opacity_off: [1, 0, { start: 1, end: 1 }],
+                aboutImg_translateY_on: [20, 0, { start: 0.8, end: 1 }],
+				aboutImg_translateY_off: [0, -20, { start: 1, end: 1 }],
+
+                aboutTxt_opacity_on : [0, 1, { start: 0.8, end: 1 }],
+                aboutTxt_opacity_off: [1, 0, { start: 1, end: 1 }],
+                aboutTxt_translateY_on: [20, 0, { start: 0.8, end: 1 }],
+				aboutTxt_translateY_off: [0, -20, { start: 1, end: 1 }],
+
             },
         },
-        {
+        {   //section02
             type:'normal',
             scrollHeight:0,
             objs:{
                 container: document.querySelector('#mainSection02'),
-                
-            }
+                // Section03 미리 불러오기
+                sectionTitle : document.querySelector('#mainSection03 .tit-box'),
+                portfolioItem : document.querySelector('#mainSection03 .main-portfolio-list'),
+            },
+            values:{
+                // Section03 미리 불러오기
+                sectionTitle_opacity_on : [0, 1, { start: 0.7, end: 1 }],
+                sectionTitle_opacity_off: [1, 0, { start: 1, end: 0.7 }],
+                sectionTitle_translateY_on: [20, 0, { start: 0.7, end: 1 }],
+				sectionTitle_translateY_off: [0, -20, { start: 1, end: 1 }],
+            },
         },
-        {
+        {   //section03
             type:'normal',
             scrollHeight:0,
             objs:{
                 container: document.querySelector('#mainSection03'),
-                bg : document.querySelector("#mainSection04 .bg"),
-                title01 : document.querySelector('#mainSection04 .tit'),
-                title02 : document.querySelector('#mainSection04 .btn-box'),
+                portfolioItem : document.querySelector('#mainSection03 .main-portfolio-list'),
+                // Section04 미리 불러오기
+                contactBg : document.querySelector('#mainSection04 .bg'),
+                contactTit : document.querySelector('#mainSection04 .tit-box'),
+                contactTxt : document.querySelector('#mainSection04 .btn-box'),
             },
             values:{
-                bg_translateX_on : [0, 100, {start:0.4, end:0.5}],
-                bg_translateX_off : [100, 0, {start:0.5, end:0.6}],
+                portfolioItem_opacity_on : [0, 1, { start: 0, end: 0.1 }],
+                portfolioItem_opacity_off: [1, 0, { start: 0, end: 0.1 }],
+                portfolioItem_translateY_on: [20, 0, { start: 0, end: 0.1 }],
+				portfolioItem_translateY_off: [0, -20, { start: 0, end: 0.1 }],
+                // Section04 미리 불러오기
+                contactBg_translateX_on: [100, 0, { start: 0.8, end: 1 }],
+                contactBg_translateX_off: [0, -20, { start: 0.8, end: 1 }],
 
-                title01_opacity_on : [0, 1, { start: 0.4, end: 0.5 }],
-                title01_opacity_off: [1, 0, { start: 0.5, end: 0 }],
+                contactTit_opacity_on : [0, 1, { start: 0.8, end: 1}],
+                contactTit_translateY_on: [10, 0, { start: 0, end: 0.2 }],
 
-                title02_opacity_on : [0, 1, { start: 0.3, end: 0.4 }],
-                title02_opacity_off: [1, 0, { start: 0.45, end: 0.5 }],
+                contactTxt_opacity_on : [0, 1, { start: 0.9, end: 1 }],
+                contactTxt_translateY_on: [10, 0, { start: 0.1, end: 0.2 }],
 
-                title01_translateY_on: [5, 0, { start: 0.4, end: 0.5 }],
-                title01_translateY_off: [0, -5, { start: 0.5, end: 0.6 }],
-
-                title02_translateY_on: [5, 0, { start: 0.3, end: 0.4 }],
-				title02_translateY_off: [0, -5, { start: 0.45, end: 0.5 }],
-            }
+            },
         },
-        {
+        {   //section04
             type:'normal',
             scrollHeight:0,
+            //heightNum : 2,
             objs:{
                 container: document.querySelector('#mainSection04'),
+            },
+            values:{
                 
             }
         },
@@ -126,9 +160,9 @@
         const currentYOffset = yOffset - prevScrollHeight;
         const scrollHeight = sectionInfo[curSection].scrollHeight;
         const scrollRatio = currentYOffset / scrollHeight;
-        console.log(scrollRatio)
+        console.log(curSection, scrollRatio);
         switch (curSection){
-            case 0 :
+            case 0 : // seciton01
                 if(scrollRatio <= 0.22){
                     objs.title01.style.opacity = calcValues(values.title01_opacity_on, currentYOffset);
                     objs.title01.style.transform = `translate3d(0, ${calcValues(values.title01_translateY_on, currentYOffset)}%, 0)`;
@@ -158,28 +192,48 @@
                     //off
                     objs.title03.style.opacity = calcValues(values.title03_opacity_off, currentYOffset);
                     objs.title03.style.transform = `translate3d(0, ${calcValues(values.title03_translateY_off, currentYOffset)}%, 0)`;
-                    
                 }
-               
+
+                // Section02 미리 불러오기
+                if(scrollRatio >= 0.7){
+                    objs.sectionTitle.style.opacity = calcValues(values.sectionTitle_opacity_on, currentYOffset);
+                    objs.sectionTitle.style.transform = `translate3d(0, ${calcValues(values.sectionTitle_translateY_on, currentYOffset)}%, 0)`;
+
+                    objs.aboutImg.style.opacity = calcValues(values.aboutImg_opacity_on, currentYOffset);
+                    objs.aboutImg.style.transform = `translate3d( -${calcValues(values.aboutImg_translateY_on, currentYOffset)}%, 0,  0)`;
+
+                    objs.aboutTxt.style.opacity = calcValues(values.aboutTxt_opacity_on, currentYOffset);
+                    objs.aboutTxt.style.transform = `translate3d( ${calcValues(values.aboutTxt_translateY_on, currentYOffset)}%, 0,  0)`;
+                }
+
+                break;
+            case 1 :
+                // Section03 미리 불러오기
+                if(scrollRatio >= 0.7){
+                    objs.sectionTitle.style.opacity = calcValues(values.sectionTitle_opacity_on, currentYOffset);
+                    objs.sectionTitle.style.transform = `translate3d(0, ${calcValues(values.sectionTitle_translateY_on, currentYOffset)}%, 0)`;
+                    objs.portfolioItem.style.opacity = 0;
+                }else{
+                    objs.sectionTitle.style.opacity = 0;
+                }
                 break;
             case 2 :
-                if(scrollRatio > 0.9){
-                    objs.bg.style.transform = `translate3d( -${calcValues(values.bg_translateX_on, currentYOffset)}%,0, 0)`;
-
-                    objs.title01.style.opacity = calcValues(values.title01_opacity_on, currentYOffset);
-                    objs.title01.style.transform = `translate3d(0, ${calcValues(values.title01_translateY_on, currentYOffset)}%, 0)`;
-                }else {
-                    //off
-                    objs.bg.style.transform = `translate3d( -${calcValues(values.bg_translateX_off, currentYOffset)}%,0, 0)`;
-
-                    //off
-                    objs.title01.style.opacity = calcValues(values.title01_opacity_off, currentYOffset);
-                    objs.title01.style.transform = `translate3d(0, ${calcValues(values.title01_translateY_off, currentYOffset)}%, 0)`;
+                if(scrollRatio >= 0){
+                    objs.portfolioItem.style.opacity = calcValues(values.portfolioItem_opacity_on, currentYOffset);
+                    objs.portfolioItem.style.transform = `translate3d(0, ${calcValues(values.portfolioItem_translateY_on, currentYOffset)}%, 0)`;
                 }
-                break;
-            case 4 :
-               
-
+                //Section04 미리 불러오기
+                if(scrollRatio >= 0.7) {
+                    objs.contactBg.style.transform = `translate3d( -${calcValues(values.contactBg_translateX_on, currentYOffset)}%,0, 0)`;
+                    objs.contactTit.style.opacity = calcValues(values.contactTit_opacity_on, currentYOffset);
+                    objs.contactTit.style.transform = `translate3d(0, ${calcValues(values.contactTit_translateY_on, currentYOffset)}%, 0)`;
+                    objs.contactTxt.style.opacity = calcValues(values.contactTxt_opacity_on, currentYOffset);
+                    objs.contactTxt.style.transform = `translate3d(0, ${calcValues(values.contactTxt_translateY_on, currentYOffset)}%, 0)`;
+                }else{
+                    objs.contactBg.style.transform = `translate3d(-100% ,0, 0)`;
+                    objs.contactTit.style.opacity = 0;
+                    objs.contactTxt.style.opacity = 0;
+                }
                 break;
         }
     }
@@ -253,12 +307,21 @@
         window.addEventListener('scroll', () =>{
             yOffset = window.pageYOffset;
             scrollLoop();
+
+            // 모바일에서 header fixed
+            if (window.innerWidth <= 800 ) {
+                if(yOffset > 0 ){
+                    document.querySelector("#header").classList.add("top-fixed");
+                }else{
+                    document.querySelector("#header").classList.remove("top-fixed");
+                }
+            }
         })
 
         window.addEventListener('resize',()=>{
-            if (window.innerWidth > 900) {
+            //if (window.innerWidth > 900) {
 				window.location.reload();
-			}
+			//}
         });
 
         window.addEventListener('orientationchange', () => {
@@ -267,9 +330,6 @@
 				window.location.reload();
 			}, 500);
   		});
-        
     }); 
-    
-
 
 }) ();
